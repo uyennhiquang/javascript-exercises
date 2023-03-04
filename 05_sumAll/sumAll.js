ac95fdf6256c75af11de361a5f78bfd287af9c24
Uyen Nhi Quang <111651301+uyennhiquang@users.noreply.github.com>
2023-03-03 21:08:39 -0500

    :)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || a < 0 || typeof b !== "number" || b < 0)
+    return "ERROR";
+  else {
+    if (a > b) return (a * (a + 1)) / 2 - (b * (b - 1)) / 2;
+    else return (b * (b + 1)) / 2 - (a * (a - 1)) / 2;
+  }
 };
 
 // Do not edit below this line
